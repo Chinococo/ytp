036b77e7d4f90682f587cb9753498db3789b2c07
Chinococo <58754099+Chinococo@users.noreply.github.com>
2021-08-21 03:01:04 +0800

    Create ytp2016final_p7.cpp

---
+++ ytp2016final_p7.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+
+#define int long long
+using namespace std;
+
+
+signed main() {
+   int a,b,c;
+   int na,nb,nc;
+   cin>>a>>b>>c;
+   na = b*6+c*8;
+   nb = a/2;
+   nc = b/2;
+   cout<<na<<" "<<nb<<" "<<nc;
+}
